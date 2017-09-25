0c798ef21d15404fc5b5736535e002ab27592ed4
ellenmb15 <ellenmb15@gmail.com>
2017-09-25 13:49:17 -0500

    Put Comment Here

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require nested_form_fields
+//= require turbolinks
 //= require_tree .
+
+
